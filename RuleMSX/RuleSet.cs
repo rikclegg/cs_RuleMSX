f21795abaa2f9ae9001e5a0e18f233a50552a7b5
Rik Clegg <rik.clegg@gmail.com>
2017-09-12 18:34:06 +0100

    First pass at full RETE implementation using OPEN set with only active rules in the set.

--- RuleMSX/RuleSet.cs
+++ RuleMSX/RuleSet.cs
@@ -1,11 +1,15 @@
-﻿namespace com.bloomberg.samples.rulemsx {
+﻿using System.Collections.Generic;
+
+namespace com.bloomberg.samples.rulemsx {
 
     public class RuleSet : RuleContainer {
 
         private string name;
+        private List<Rule> open;
 
         internal RuleSet(string name) {
             this.name = name;
+            this.open = new List<Rule>();
         }
 
         public string getName() {
@@ -13,21 +17,27 @@
         }
 
         public void execute(DataSet dataSet) {
-            executeRules(this, dataSet);
+
+            // Move Alpha rules into open
+            foreach (Rule r in this.rules) {
+                open.Add(r);
+            }
+
+            while(open.Count > 0) executeOpen(open, dataSet);
         }
 
-        private void executeRules(RuleContainer source, DataSet dataSet) {
+        private void executeOpen(List<Rule> source, DataSet dataSet) {
 
-            foreach (Rule r in source.rules) {
+            List<Rule> newOpen = new List<Rule>();
 
+            foreach (Rule r in source) {
                 if (r.GetEvaluator().Evaluate(dataSet)) {
-
-                    foreach (RuleAction a in r.GetActions()) {
-                        a.Execute(dataSet);
-                    }
-                    executeRules(r, dataSet);
+                    foreach (Rule c in r.rules) newOpen.Add(c);
+                    foreach (RuleAction a in r.GetActions()) a.Execute(dataSet);
                 }
             }
+
+            this.open = newOpen;
         }
     }
 }
