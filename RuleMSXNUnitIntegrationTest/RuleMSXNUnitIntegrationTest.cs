54ea9aae08e8fcc775c4a203f87fdfca47761ec0
rclegg2 <rclegg2@bloomberg.net>
2017-09-26 16:37:13 +0100

    Add Integration Tests

---
+++ RuleMSXNUnitIntegrationTest/RuleMSXNUnitIntegrationTest.cs
@@ -0,0 +1,71 @@
+﻿using com.bloomberg.samples.rulemsx;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace RuleMSXNUnitIntegrationTest
+{
+    [TestFixture]
+    public class RuleMSXNUnitIntegrationTest
+    {
+        [Test]
+        public void SingleRuleITest()
+        {
+            RuleMSX rmsx = new RuleMSX();
+
+            string newRuleSetName = "SingleIterationRuleSet";
+            string newDataSetName = "SingleIterationDataSet";
+            string newRuleName = "IsBooleanTrue";
+            string actionMessage = "ActionMessage";
+
+            RuleSet rs = rmsx.createRuleSet(newRuleSetName);
+            DataSet ds = rmsx.createDataSet(newDataSetName);
+
+            RuleAction rai = new GenericAction(actionMessage);
+
+            Rule r = new Rule(newRuleName, new GenericBoolRule(true), rai);
+            rs.AddRule(r);
+
+            rs.Execute(ds);
+
+            GenericAction rao = (GenericAction)rai;
+            Assert.That(rao.getOutgoing, Is.EqualTo(actionMessage));
+        }
+
+        private class GenericBoolRule : RuleEvaluator
+        {
+            bool ret;
+
+            public GenericBoolRule(bool returnValue)
+            {
+                this.ret = returnValue;
+            }
+
+            public override bool Evaluate(DataSet dataSet)
+            {
+                return this.ret;
+            }
+        }
+
+        private class GenericAction : RuleAction
+        {
+            string message;
+            string outgoing;
+
+            public GenericAction(string message)
+            {
+                this.message = message;
+            }
+            public void Execute(DataSet dataSet)
+            {
+                this.outgoing = message;
+            }
+
+            public string getOutgoing()
+            {
+                return this.outgoing;
+            }
+        }
+
+    }
+}
